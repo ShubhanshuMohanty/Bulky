de5b43ceeb37fb86eb6f15a110606d15dc28f223
shubhanshu <shubhanshumohanty@gmail.com>
2025-06-10 20:53:36 +0530

    set connection string and add categories model

---
+++ Bulky/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bulky.Models;
+namespace Bulky.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        //ctor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
