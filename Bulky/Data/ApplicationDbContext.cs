f275c1724f02e20108146c0a19c818ea6401fcce
shubhanshu <shubhanshumohanty@gmail.com>
2025-06-11 16:36:50 +0530

    complete category crude and include toaster for notifications

--- Bulky/Data/ApplicationDbContext.cs
+++ Bulky/Data/ApplicationDbContext.cs
@@ -11,5 +11,16 @@ namespace Bulky.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        //data insert kar raha hu or onModelCreating method ko override kar raha hu 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seed data
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Adventure", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Romance", DisplayOrder = 3 }
+            );
+        }
     }
 }
