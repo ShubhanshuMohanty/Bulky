5e00b4bb0bc8b125f1cae08f08b65699b66c3f2a
shubhanshu <shubhanshumohanty@gmail.com>
2025-06-29 13:57:17 +0530

    order confirmation withot payment intigration

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace Bulky.DataAccess.Data
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         //data insert kar raha hu or onModelCreating method ko override kar raha hu 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
